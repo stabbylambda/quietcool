9853060a2fc7f2380a18e57114397b59a3ad8cf5
David Stone <david@stabbylambda.com>
2017-09-06 10:24:38 -0700

    Add the ability to refresh & quit

--- index.js
+++ index.js
@@ -20,40 +20,55 @@ const formatFanName = ({
 };
 
 const mainMenu = (ip) => {
-return Rx.Observable.of(ip)
-    .flatMap(ip => fanControl.listFans(ip))
-    .flatMap(fans => {
-        let fanCount = fans.length;
-        console.log(`Found ${fanCount} fans`);
-        return Rx.Observable.from(fans)
-            .concatMap(fan => Rx.Observable.of(fan).delay(100))
-            .flatMap(fan => fanControl.getFanInfo(ip, fan.uid))
-            .flatMap(info => fanControl.getFanStatus(ip, info.uid).map(status => ({
-                status,
-                info
-            })))
-            .take(fanCount)
-            .reduce((acc, fan) => {
-                acc[fan.info.uid] = fan;
-                return acc;
-            }, {});
-    })
-    .flatMap(fans => enquirer.ask([{
-        type: "list",
-        name: "selectedFan",
-        message: "Here are your fans",
-        choices: _.map(fans, formatFanName),
-        transform: answer => _.find(fans, x => answer === formatFanName(x))
-    }]));
+    return Rx.Observable.of(ip)
+        .flatMap(ip => fanControl.listFans(ip))
+        .flatMap(fans => {
+            let fanCount = fans.length;
+            console.log(`Found ${fanCount} fans`);
+            return Rx.Observable.from(fans)
+                .concatMap(fan => Rx.Observable.of(fan).delay(100))
+                .flatMap(fan => fanControl.getFanInfo(ip, fan.uid))
+                .flatMap(info => fanControl.getFanStatus(ip, info.uid).map(status => ({
+                    status,
+                    info
+                })))
+                .take(fanCount)
+                .reduce((acc, fan) => {
+                    acc[fan.info.uid] = fan;
+                    return acc;
+                }, {});
+        })
+        .flatMap(fans => {
+            let choices = _.map(fans, formatFanName);
+
+            return enquirer.ask([{
+                type: "list",
+                name: "mainMenu",
+                message: "Here are your fans",
+                choices: [
+                    ...choices,
+                    enquirer.separator(),
+                    "Refresh",
+                    "Quit"
+                ],
+                transform: answer => {
+                    if(answer == "Refresh" || answer == "Quit") {
+                        return {type: "action", value: answer};
+                    }
+                    let fan = _.find(fans, x => answer === formatFanName(x));
+                    return {type: "fan", value: fan};
+                }
+            }]);
+        });
 };
 
 const speedMenu = (ip, answers) => {
-    let uid = answers.selectedFan.info.uid;
+    let uid = answers.mainMenu.value.info.uid;
 
     return Rx.Observable.from(enquirer.ask([{
         type: "list",
         name: "setSpeed",
-        message: `What speed for ${answers.selectedFan.info.name}`,
+        message: `What speed for ${answers.mainMenu.value.info.name}`,
         choices: ["High", "Medium", "Low"]
     }]))
         .flatMap(answers => {
@@ -65,24 +80,33 @@ const speedMenu = (ip, answers) => {
             case "Low":
                 return fanControl.setCurrentSpeed(ip, uid, 1);
             }
-        }).flatMap(x => program(ip));
+        })
+        .flatMap(x => program(ip));
 }
 
 const program = (ip) => {
-   return mainMenu(ip)
-        .flatMap(selected => enquirer.ask([{
-            type: "list",
-            name: "action",
-            message: "What do you want to do?",
-            choices: ["Turn On", "Turn Off", "Set Speed", "Update Name", "Update Speed", "Back To Menu"]
-        }]))
+    return mainMenu(ip)
+        .takeWhile(answer => answer.mainMenu.value != "Quit")
+        .flatMap(({mainMenu: {type, value}}) => {
+            if(type == "action" && value == "Refresh") {
+                return program(ip);
+            } else {
+                return enquirer.ask([{
+                    type: "list",
+                    name: "action",
+                    message: "What do you want to do?",
+                    choices: ["Turn On", "Turn Off", "Set Speed", "Update Name", "Update Speed", "Back To Menu"]
+                }]);
+            }
+        })
         .flatMap(answers => {
+            let uid = answers.mainMenu.value.info.uid;
             switch(answers.action) {
             case "Turn On":
-                return fanControl.turnFanOn(ip, answers.selectedFan.info.uid)
+                return fanControl.turnFanOn(ip, uid)
                     .flatMap(x => program(ip));
             case "Turn Off":
-                return fanControl.turnFanOff(ip, answers.selectedFan.info.uid)
+                return fanControl.turnFanOff(ip, uid)
                     .flatMap(x => program(ip));
             case "Set Speed":
                 return speedMenu(ip, answers);
@@ -93,8 +117,13 @@ const program = (ip) => {
             }
         });
 };
-program(process.env.CONTROLLER_IP)
-    .catch(err => program(ip))
+let ip = process.env.CONTROLLER_IP;
+program(ip)
+    .catch(err => {
+        console.log("ERROR", err);
+        program(ip);
+
+    })
     .subscribe(
         fans => console.log(fans),
         err => console.log(err),
